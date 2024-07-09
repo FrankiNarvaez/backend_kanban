8efa8449e712654ce1fbd90f26b3946fd8d47148
FrankiNarvaez <frannki03@gmail.com>
2024-07-08 22:02:39 -0500

    Delete structure of JS

--- src/database/connection.database.js
+++
@@ -1,9 +0,0 @@
-import pg from 'pg'
-import 'dotenv/config'
-
-const connectionString = process.env.DATABASE_URL
-
-export const pool = new pg.Pool({
-  allowExitOnIdle: true,
-  connectionString
-})
