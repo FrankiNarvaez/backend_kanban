c12d05ec84aad3f4340d21cad001585177938250
VegaSyd <nicolas.vega@utp.edu.co>
2024-07-11 00:27:42 -0500

    added sql querys for update the section_id

--- db/create-table.sql
+++ db/create-table.sql
@@ -5,7 +5,8 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL, -- El campo debe ser de 50 caracteres o menos
     email VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(120) NOT NULL
+    password VARCHAR(120) NOT NULL,
+    id_theme BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Crear tabla de categorías
@@ -13,7 +14,8 @@ CREATE TABLE section (
     section_id SERIAL PRIMARY KEY,
     section_name VARCHAR(50) NOT NULL, -- El campo debe ser de 50 caracteres o menos
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) -- Referencia a la clave foranea en user_id
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    index_section SERIAL  -- Referencia a la clave foranea en user_id
 );
 
 -- Crear tabla de descripciones
@@ -22,5 +24,6 @@ CREATE TABLE tasks (
     tasks_name VARCHAR(50) NOT NULL, -- El campo debe ser de 50 caracteres o menos
     tasks TEXT NOT NULL, -- TEXT permite crear guardar datos de datos de tamaño indefinido
     section_id INTEGER NOT NULL     ,
-    FOREIGN KEY (section_id) REFERENCES section(section_id) ON DELETE CASCADE ON UPDATE CASCADE -- Referencia a la clave foranea en section_id
+    FOREIGN KEY (section_id) REFERENCES section(section_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    index_task SERIAL -- Referencia a la clave foranea en section_id
 );
