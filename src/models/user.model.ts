aee810a6fbac14b42ef59c4025efd663660d212a
davidpatjus <david.patino1@utp.edu.co>
2024-07-11 10:53:34 -0500

    changes done on sectionUpdate and taskUpdate

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -23,13 +23,13 @@ const getSections = async (userId: number): Promise<Section[]> => {
 }
 
 // Get a user's section from the database
-const getSection = async (sectionId: number): Promise <Section> => {
+const getSection = async (sectionId: number): Promise<Section> => {
   const { rows } = await pool.query('SELECT * FROM section WHERE section_id = $1', [sectionId])
   return rows[0]
 }
 
 // Get a user's section's tasks from the database
-const getTasks = async (userId: number, sectionId: number): Promise<Task []> => {
+const getTasks = async (userId: number, sectionId: number): Promise<Task[]> => {
   const { rows } = await pool.query(`SELECT tasks.task_id, tasks.description, section.section_name 
     FROM tasks 
     JOIN section ON tasks.section_id = section.section_id
@@ -39,7 +39,7 @@ const getTasks = async (userId: number, sectionId: number): Promise<Task []> => 
 }
 
 // Get a user's section's task from the database
-const getTask = async (taskId: number): Promise <Task> => {
+const getTask = async (taskId: number): Promise<Task> => {
   const { rows } = await pool.query('SELECT * FROM tasks WHERE task_id = $1', [taskId])
   return rows[0]
 }
@@ -51,7 +51,7 @@ const createSection = async ({ userId, sectionName }: { userId: number, sectionN
 }
 
 // Create a task in the database, on the tasks table that is related to a section
-const createTask = async ({ sectionId, taskName }: { sectionId: number, taskName: string }): Promise <Task> => {
+const createTask = async ({ sectionId, taskName }: { sectionId: number, taskName: string }): Promise<Task> => {
   const { rows } = await pool.query('INSERT INTO task (section_id, task_name) VALUES ($1, $2) RETURNING *', [sectionId, taskName])
   return rows[0]
 }
@@ -80,13 +80,13 @@ const updateNameTask = async (taskId: number, taskName: string): Promise<void> =
 const updateSectionIdTask = async (taskId: number, startIndex: number, endIndex: number, sectionId: number): Promise<void> => {
   await pool.query('UPDATE tasks SET section_id = $1 WHERE task_id = $2', [sectionId, taskId])
   // -- Se cambia el index de la sección 4 al index de la sección 2
-  await pool.query ('UPDATE tasks SET index_task = $2 WHERE taskId = $1', [taskId, endIndex]); 
+  await pool.query('UPDATE tasks SET index_task = $2 WHERE taskId = $1', [taskId, endIndex])
   if (startIndex > endIndex) {
-      // Se incrementa el index de las secciones que estén a la derecha de la tarea 2 expetuando la tarea 4
-      await pool.query ('UPDATE section SET index_task = index_task+1 WHERE index_task >= $2 AND task_id != $1', [taskId, endIndex])
-  }else {
-      // Se reduce el index de las secciones que estén a la izquierda de la tarea 2 expetuando la tarea 4
-      await pool.query ('UPDATE section SET index_task = index_task-1 WHERE index_task BETWEEN $2 AND $3 AND task_id != $1', [taskId, startIndex, endIndex])
+    // Se incrementa el index de las secciones que estén a la derecha de la tarea 2 expetuando la tarea 4
+    await pool.query('UPDATE section SET index_task = index_task+1 WHERE index_task >= $2 AND task_id != $1', [taskId, endIndex])
+  } else {
+    // Se reduce el index de las secciones que estén a la izquierda de la tarea 2 expetuando la tarea 4
+    await pool.query('UPDATE section SET index_task = index_task-1 WHERE index_task BETWEEN $2 AND $3 AND task_id != $1', [taskId, startIndex, endIndex])
   }
 }
 
